21a4fe155ad22d2fe73ee62986ca9c9d8db49f17
vishnu <talakokkula.vishnu2004@gmail.com>
2024-01-06 22:01:22 +0530

    changes

--- WebDev/BackEnd/JS/first.js
+++ WebDev/BackEnd/JS/first.js
@@ -1,2 +1,11 @@
-var myName = "angela";
-console.log(myname)
+var a = 1;
+var b = 2;
+console.log(a);
+console.log(b);
+// Strings Concatenation in JS
+var fName = "angela";
+var lname = "yu";
+console.log(fName.length);
+console.log(fName+" "+lname);
+
+ 

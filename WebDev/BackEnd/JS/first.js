ea088f398efe0d8f2af5dc459ea2c11bbbcba483
vishnu <talakokkula.vishnu2004@gmail.com>
2024-01-08 15:19:26 +0530

    changes

--- WebDev/BackEnd/JS/first.js
+++ WebDev/BackEnd/JS/first.js
@@ -1,4 +1,4 @@
-var a = 1;
+var a = 10;
 var b = 2;
 console.log(a);
 console.log(b);
@@ -10,5 +10,23 @@ console.log(fName+" "+lname);
 console.log(fName.slice(0,2));
 console.log(fName.toUpperCase());
 console.log(fName.toLowerCase());
-
- 
+// Arthimetic Operations in JS
+console.log("Arthimetic Operations");
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+// Expressions in JS
+var res = 3*5+2;
+console.log(res);
+var dog = 5;
+var human = (dog-2)*4+21;
+console.log(human)
+var c = 9
+var d = 10
+c++;    //Increment
+console.log(c);
+c--;
+console.log(c); // Decrement
+console.log(c+=d);

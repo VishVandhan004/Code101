881e8947d3311dc0b68b93da32afa57680c1fd4b
vishnu <talakokkula.vishnu2004@gmail.com>
2024-01-08 13:19:10 +0530

    changes

--- WebDev/BackEnd/JS/first.js
+++ WebDev/BackEnd/JS/first.js
@@ -2,11 +2,13 @@ var a = 1;
 var b = 2;
 console.log(a);
 console.log(b);
-// Strings Concatenation in JS
-var fName = "angela";
+// Strings in JS
+var fName = "Angela";
 var lname = "yu";
 console.log(fName.length);
 console.log(fName+" "+lname);
 console.log(fName.slice(0,2));
+console.log(fName.toUpperCase());
+console.log(fName.toLowerCase());
 
  

1ce9cb0a9b3e495a5b98080fe03d107cbf1cc4cd
vishnu <talakokkula.vishnu2004@gmail.com>
2024-01-09 10:42:41 +0530

    changes

--- WebDev/BackEnd/JS/first.js
+++ WebDev/BackEnd/JS/first.js
@@ -6,22 +6,22 @@ console.log(b);
 var fName = "Angela";
 var lname = "yu";
 console.log(fName.length);
-console.log(fName+" "+lname);
-console.log(fName.slice(0,2));
+console.log(fName + " " + lname);
+console.log(fName.slice(0, 2));
 console.log(fName.toUpperCase());
 console.log(fName.toLowerCase());
 // Arthimetic Operations in JS
 console.log("Arthimetic Operations");
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
-console.log(a%b);
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
 // Expressions in JS
-var res = 3*5+2;
+var res = 3 * 5 + 2;
 console.log(res);
 var dog = 5;
-var human = (dog-2)*4+21;
+var human = (dog - 2) * 4 + 21;
 console.log(human)
 var c = 9
 var d = 10
@@ -29,29 +29,40 @@ c++;    //Increment
 console.log(c);
 c--;
 console.log(c); // Decrement
-console.log(c+=d);
+console.log(c += d);
 // Functions in JS
-function hello(){       // defining a function
-    console.log("hello bsdk");
+function hello() {       // defining a function
+  console.log("hello bsdk");
 }
 hello();   // calling the function
-function getMilk() {   
-    console.log("leaveHouse");
-    console.log("moveRight");
-    console.log("moveRight");
-    console.log("moveUp");
-    console.log("moveUp");
-    console.log("moveUp");
-    console.log("moveUp");
-    console.log("moveRight");
-    console.log("moveRight");
-    console.log("moveLeft");
-    console.log("moveLeft");
-    console.log("moveDown");
-    console.log("moveDown");
-    console.log("moveDown");
-    console.log("moveDown");
-    console.log("moveLeft");
-    console.log("moveLeft");
-    console.log("enterHouse");
-  }
+function getMilk(bottles) {
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("buy " + bottles + " bottles of milk");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+}
+getMilk(20);
+function lifeInWeeks(age) {
+  var left = 90 - age;
+  var leftday = 365 * left;
+  var leftweek = 52 * left;
+  var leftmonth = 12 * left;
+  console.log("You have " + leftday + " days, " + leftweek + " weeks, and " + leftmonth + " months left.");
+}
+lifeInWeeks(56);
+

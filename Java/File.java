2e77ee060601f54646e16ad3650148e3d4871838
Vishnu Vandhan Talakokkula <talakokkula.vishnu2004@gmail.com>
2024-02-06 22:20:45 +0530

    changes

--- Java/File.java
+++ Java/File.java
@@ -1,34 +1,43 @@
+/*Given r red, b blue, and g green balls, find the total number of arrangements
+in a row such that no two balls of the same color end up together.
+input format : three integers (first integer is for r second is b and last is for g)
+output format : integer number
+
+Input:1  2 1
+Output:6
+The arrangements are [bgbr, bgrb, brbg, brgb, gbrb, rbgb]
+
+Input:  2 3 1
+Output:10
+The arrangements are [bgbrbr, bgrbrb, brbgbr, brbgrb, brbrbg, brbrgb, 
+brgbrb, gbrbrb, rbgbrb, rbrbgb]
+*/
+
 import java.util.*;
-
-class File {
-    public static boolean bs(int arr[], int target) {
-        int low = 0;
-        int high = arr.length - 1;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (target == arr[mid]) {
-                return true;
-            } else if (target < arr[mid]) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
+public class File {
+    public static int f(int r, int b, int g, char j, HashMap<String, Integer> memo) {
+        String key = r + "" + b + "" + g + "*" + j;
+        if (memo.containsKey(key)) {
+            return memo.get(key);
         }
-        return false;
+        if (r < 0 || b < 0 || g < 0) return 0;
+        if (r == 0 && g == 0 && b == 0) return 1;
+        int ways = 0;
+        if (j == 'r') ways = f(r, b - 1, g, 'b', memo) + f(r, b, g - 1, 'g', memo);
+        else if (j == 'b') ways = f(r - 1, b, g, 'r', memo) + f(r, b, g - 1, 'g', memo);
+        else if (j == 'g') ways = f(r - 1, b, g, 'r', memo) + f(r, b - 1, g, 'b', memo);
+        memo.put(key, ways);
+        return ways;
     }
-
+    public static int run(int r, int b, int g) {
+        HashMap<String, Integer> memo = new HashMap<>();
+        return f(r - 1, b, g, 'r', memo) + f(r, b - 1, g, 'b', memo) + f(r, b, g - 1, 'g', memo);
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Size: ");
-        int n = sc.nextInt();
-        int arr[] = new int[n];
-        System.out.println("Enter Array elements: ");
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-        System.out.println("Enter Target: ");
-        int target = sc.nextInt();
-        System.out.println(bs(arr, target));
-        sc.close();
+        int r = sc.nextInt();
+        int b = sc.nextInt();
+        int g = sc.nextInt();
+        System.out.print(run(r, b, g));
     }
 }

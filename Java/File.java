c8aedd16ac581dd025285baf66e76fc34f469d34
vishnu <talakokkula.vishnu2004@gmail.com>
2024-04-19 22:12:25 +0530

    changes

--- Java/File.java
+++ Java/File.java
@@ -1,19 +1,9 @@
 import java.util.*;
 class File {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int arr[] = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-        int k = sc.nextInt();
-        for(int i=k;i<n;i++){
-            System.out.println(arr[i]+" ");
-        }
-        for(int i=0;i<k;i++){
-            System.out.println(arr[i]+" ");
-        }
-        sc.close();
+    public static void main (String[] args) {
+        int i=3;
+        System.out.println(i++);
+        System.out.println(i);
+        System.out.println(++i);
     }
 }

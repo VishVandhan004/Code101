4e74aa4bc1e19f1053477d1269c8c9e260a05056
vishnu <talakokkula.vishnu2004@gmail.com>
2024-05-17 21:40:22 +0530

    changes

--- Java/File.java
+++ Java/File.java
@@ -1,9 +1,19 @@
 import java.util.*;
 class File {
     public static void main (String[] args) {
-        int i=3;
-        System.out.println(i++);
-        System.out.println(i);
-        System.out.println(++i);
+        Scanner sc = new Scanner(System.in);
+        double x = sc.nextDouble();
+        int n = sc.nextInt();
+        System.out.println(String.format("%.5f",power(x,n)));
+    }
+    public static double power(double x, int n){
+        // double val=1.00000;
+        if(n==0){
+            return 1;
+        }
+        if(n<0){
+            return (1/x*power(x,n+1));
+        }
+        return x*power(x,n-1);
     }
 }

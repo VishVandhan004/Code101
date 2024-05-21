3e26c214319ebc58ef88d9c9ff58acfda14ca6b3
Vishnu Vandhan Talakokkula <talakokkula.vishnu2004@gmail.com>
2024-05-21 07:19:00 +0530

    changes

--- Java/File.java
+++ Java/File.java
@@ -2,18 +2,26 @@ import java.util.*;
 class File {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int arr[] = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+        System.err.println(check(s1,s2));
+    }
+
+public static boolean check(String s1, String s2){
+    int len1 = s1.length();
+    int len2 = s2.length();
+    if(len1 != len2){
+        return false;
+    }
+    if(len1 == len2){
+        for(int i=0;i<len1;i++){
+            for(int j=0;j<len2;j++){
+                if(s1.charAt(i)==s2.charAt(j)){
+                    return false;
+                }
+            }
         }
-        int k = sc.nextInt();
-        for(int i=k;i<n;i++){
-            System.out.println(arr[i]+" ");
-        }
-        for(int i=0;i<k;i++){
-            System.out.println(arr[i]+" ");
-        }
-        sc.close();
+    }
+    return true; 
     }
 }

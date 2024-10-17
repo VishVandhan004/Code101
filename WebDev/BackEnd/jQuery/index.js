0ad0cae3ca4cf1671b3e70320dd1d19d4726b52c
Vishnu Vandhan Talakokkula <talakokkula.vishnu2004@gmail.com>
2024-10-17 11:54:31 +0530

    changes

--- WebDev/BackEnd/jQuery/index.js
+++ WebDev/BackEnd/jQuery/index.js
@@ -4,4 +4,7 @@
 
 $("h1").addClass("big-title margin-50");
 $("h1").text("10GEY");
-$("button").text("vadiley bey");
+// $("button").text("vadiley bey"); 
+$("button").html("<em>Vadiley ra ninja</em>");
+
+$("a").attr("href","https://www.espncricinfo.com/"); // selecting the anchor tag and changing the attribute href to espncricinfo

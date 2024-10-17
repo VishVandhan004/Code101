595fe9bf94c0ddab6e4c38faba5be25fb3ca4ee1
Vishnu Vandhan Talakokkula <talakokkula.vishnu2004@gmail.com>
2024-10-17 11:16:44 +0530

    changes

--- WebDev/BackEnd/jQuery/index.js
+++ WebDev/BackEnd/jQuery/index.js
@@ -1,1 +1,7 @@
-alert("working")
+// $(document).ready(function(){ 
+//     $("h1").css("color","red");
+// }) // it targets the h1 of the document and changes the color to red 
+
+$("h1").addClass("big-title margin-50");
+$("h1").text("10GEY");
+$("button").text("vadiley bey");

bb639d09fdf8cb9ead925406cbf4019644ff0467
Vishnu Vandhan Talakokkula <talakokkula.vishnu2004@gmail.com>
2024-11-02 13:49:14 +0530

    changes

--- WebDev/BackEnd/Express.js/3.2_HTTP_Requests/index.js
+++ WebDev/BackEnd/Express.js/3.2_HTTP_Requests/index.js
@@ -2,15 +2,18 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => { // goes to end of the home page
+app.get("/", (req, res) => { 
+  // goes to end of the home page
   res.send("<h1>Hello</h1>");
 });
 
 app.get("/about", (req, res) => {
+  // when you type localhost:3000/about, you get About me
   res.send("<h1>About Me</h1><p>My name is Angela</p>");
 });
 
 app.get("/contact", (req, res) => {
+  // when you type localhost:3000/contact, you get Contact me
   res.send("<h1>Contact Me</h1><p>Phone: +44123456789</p>");
 });
 

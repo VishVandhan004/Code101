1c0edabf3345379cae57a422d74230f9d9ac4589
Vishnu Vandhan Talakokkula <talakokkula.vishnu2004@gmail.com>
2024-12-21 12:45:48 +0530

    changes

--- WebDev/BackEnd/EJS/2/index.js
+++ WebDev/BackEnd/EJS/2/index.js
@@ -1,13 +1,14 @@
 import express from "express";
+
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
   const data = {
-    title: "EJS Tags",
+    title: "EJS Tags",       // items are given as variables and then rendered to the .ejs files...
     seconds: new Date().getSeconds(),
     items: ["apple", "banana", "cherry"],
-    htmlContent: "<strong>This is some strong text</strong>",
+    htmlContent: "<em>This is some em text</em>",
   };
   res.render("index.ejs", data);
 });

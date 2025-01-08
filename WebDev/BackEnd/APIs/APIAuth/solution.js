627d46155e106316668bc4ccff3b4c70bbcf2456
Vishnu Vandhan Talakokkula <talakokkula.vishnu2004@gmail.com>
2025-01-08 13:36:12 +0530

    changes

--- WebDev/BackEnd/APIs/APIAuth/solution.js
+++ WebDev/BackEnd/APIs/APIAuth/solution.js
@@ -1,6 +1,7 @@
 import express from "express";
 import axios from "axios";
 
+// import the express app and API URL.
 const app = express();
 const port = 3000;
 const API_URL = "https://secrets-api.appbrewery.com";
@@ -15,15 +16,18 @@ app.get("/", (req, res) => {
   res.render("index.ejs", { content: "API Response." });
 });
 
+// if there no authentication , we use axios to send requests to another server API URL at random endpoint.
 app.get("/noAuth", async (req, res) => {
   try {
     const result = await axios.get(API_URL + "/random");
+    // we use stringify to get the data in form of JSON.
     res.render("index.ejs", { content: JSON.stringify(result.data) });
   } catch (error) {
     res.status(404).send(error.message);
   }
 });
 
+// in basic auth, we add the username and password as parameters. all the base64 encoding will be done here. result will be in JSON format.
 app.get("/basicAuth", async (req, res) => {
   try {
     const result = await axios.get(API_URL + "/all?page=2", {
@@ -38,6 +42,7 @@ app.get("/basicAuth", async (req, res) => {
   }
 });
 
+// in APIKEY auth, we send the API key and score as parameters using axios. it will be converted to JSON format.
 app.get("/apiKey", async (req, res) => {
   try {
     const result = await axios.get(API_URL + "/filter", {
@@ -51,6 +56,8 @@ app.get("/apiKey", async (req, res) => {
     res.status(404).send(error.message);
   }
 });
+
+// in bearer token, we provide config to the header and give the id as 2.
 
 const config = {
   headers: { Authorization: `Bearer ${yourBearerToken}` },

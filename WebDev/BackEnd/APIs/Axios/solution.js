3f66022e6bc5d1c0fea2a8928e57cbcd3ef01e68
Vishnu Vandhan Talakokkula <talakokkula.vishnu2004@gmail.com>
2025-01-08 12:02:31 +0530

    changes

--- WebDev/BackEnd/APIs/Axios/solution.js
+++ WebDev/BackEnd/APIs/Axios/solution.js
@@ -2,18 +2,24 @@ import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
 
+// import the express app
 const app = express();
 const port = 3000;
 
+// we use the body parser middleware
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Axios code
+// as soon as you open the website, we send GET request to the website and it generates a random info at starting. as 'random' is the endpoint to the API. then the user can select an option from the dropdown to get the required punchcards by using POST requests.
 app.get("/", async (req, res) => {
   try {
     const response = await axios.get("https://bored-api.appbrewery.com/random");
+    // we are generating random activties. so we are collecting the data from the above line.
     const result = response.data;
     console.log(result);
     res.render("solution.ejs", { data: result });
+    // after collecting the data, we need to send the data to the solution.ejs file in the form as key:value pair of data object
   } catch (error) {
     console.error("Failed to make request:", error.message);
     res.render("solution.ejs", {
@@ -21,6 +27,7 @@ app.get("/", async (req, res) => {
     });
   }
 });
+
 
 app.post("/", async (req, res) => {
   try {

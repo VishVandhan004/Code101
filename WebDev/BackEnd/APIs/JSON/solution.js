d828359d31ff70d74121700eb2a72d7ab9158c22
Vishnu Vandhan Talakokkula <talakokkula.vishnu2004@gmail.com>
2025-01-08 11:22:12 +0530

    changes

--- WebDev/BackEnd/APIs/JSON/solution.js
+++ WebDev/BackEnd/APIs/JSON/solution.js
@@ -1,25 +1,32 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+// import the express app
 const app = express();
 const port = 3000;
 
+// we have the JSON variable which has the JSON.
 const recipeJSON =
   '[{"id": "0001","type": "taco","name": "Chicken Taco","price": 2.99,"ingredients": {"protein": {"name": "Chicken","preparation": "Grilled"},  "salsa": {"name": "Tomato Salsa","spiciness": "Medium"},  "toppings": [{"name": "Lettuce",  "quantity": "1 cup",  "ingredients": ["Iceberg Lettuce"]  },      {"name": "Cheese",  "quantity": "1/2 cup",  "ingredients": ["Cheddar Cheese", "Monterey Jack Cheese"]  },      {"name": "Guacamole",  "quantity": "2 tablespoons",  "ingredients": ["Avocado", "Lime Juice", "Salt", "Onion", "Cilantro"]  },      {"name": "Sour Cream",  "quantity": "2 tablespoons",  "ingredients": ["Sour Cream"]  }      ]    }  },{"id": "0002","type": "taco","name": "Beef Taco","price": 3.49,"ingredients": {"protein": {"name": "Beef","preparation": "Seasoned and Grilled"},  "salsa": {"name": "Salsa Verde","spiciness": "Hot"},  "toppings": [{"name": "Onions",  "quantity": "1/4 cup",  "ingredients": ["White Onion", "Red Onion"]  },      {"name": "Cilantro",  "quantity": "2 tablespoons",  "ingredients": ["Fresh Cilantro"]  },      {"name": "Queso Fresco",  "quantity": "1/4 cup",  "ingredients": ["Queso Fresco"]  }      ]    }  },{"id": "0003","type": "taco","name": "Fish Taco","price": 4.99,"ingredients": {"protein": {"name": "Fish","preparation": "Battered and Fried"},  "salsa": {"name": "Chipotle Mayo","spiciness": "Mild"},  "toppings": [{"name": "Cabbage Slaw",  "quantity": "1 cup",  "ingredients": [    "Shredded Cabbage",    "Carrot",    "Mayonnaise",    "Lime Juice",    "Salt"          ]  },      {"name": "Pico de Gallo",  "quantity": "1/2 cup",  "ingredients": ["Tomato", "Onion", "Cilantro", "Lime Juice", "Salt"]  },      {"name": "Lime Crema",  "quantity": "2 tablespoons",  "ingredients": ["Sour Cream", "Lime Juice", "Salt"]  }      ]    }  }]';
 
+// body-parser middleware
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 let data;
 
+// the data obtained from the switch statements will be render to solution.ejs, here key is recipe and value is data.
 app.get("/", (req, res) => {
   res.render("solution.ejs", { recipe: data });
 });
 
+// POST request after the user selects any one of the 3 options ,so use the switch case
 app.post("/recipe", (req, res) => {
   switch (req.body.choice) {
     case "chicken":
+      // there are 3 objects here, chicken(0), beef(1), fish(2). so allocate the options accordingly.
       data = JSON.parse(recipeJSON)[0];
+      // in the above line, we are converting the JSON to JS Object..
       break;
     case "beef":
       data = JSON.parse(recipeJSON)[1];

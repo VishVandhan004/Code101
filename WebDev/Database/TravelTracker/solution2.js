d21b8e565af9f15458be2008432e49c8f1469099
Vishnu Vandhan Talakokkula <talakokkula.vishnu2004@gmail.com>
2025-01-20 15:56:54 +0530

    changes

--- WebDev/Database/TravelTracker/solution2.js
+++ WebDev/Database/TravelTracker/solution2.js
@@ -34,22 +34,24 @@ app.get("/", async (req, res) => {
 });
 
 //INSERT new country
+// in the index.ejs, we got a POST request to the /add route 
 app.post("/add", async (req, res) => {
+  // the input typed by the user which is the name of a country
   const input = req.body["country"];
-
+  // the country typed by the user will be sent here and the country code will be looked using the query. exp, if country is France, FR will be searched and kept in the result variable.
   const result = await db.query(
     "SELECT country_code FROM countries WHERE country_name = $1",
-    [input]
+    [input] // country name will be kept here..
   );
-
+// if there is data available, we continue and grab the country code
   if (result.rows.length !== 0) {
     const data = result.rows[0];
     const countryCode = data.country_code;
-
+// after grabbong the country code , we also insert that value into the visited_countries table as well..
     await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [
       countryCode,
     ]);
-    res.redirect("/");
+    res.redirect("/");// redirect to home page..
   }
 });
 

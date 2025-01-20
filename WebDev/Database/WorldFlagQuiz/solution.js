51ac765d2c4cc0b924be0390da19b6ea81d29a01
Vishnu Vandhan Talakokkula <talakokkula.vishnu2004@gmail.com>
2025-01-20 12:50:46 +0530

    changes

--- WebDev/Database/WorldFlagQuiz/solution.js
+++ WebDev/Database/WorldFlagQuiz/solution.js
@@ -1,26 +1,29 @@
+// import the express app and postgres packages
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 
+// postgres syntax
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "world",
-  password: "123456",
-  port: 5432,
+  password: "postgresql2004@",
+  port: 5432, // this is default, don't change it..
 });
 
+// express app
 const app = express();
 const port = 3000;
-
+// used for database connection...
 db.connect();
-
+// all the flags are stored in an array..
 let quiz = [];
 db.query("SELECT * FROM flags", (err, res) => {
   if (err) {
     console.error("Error executing query", err.stack);
   } else {
-    quiz = res.rows;
+    quiz = res.rows; // the data will be selected in rows
   }
   db.end();
 });
@@ -42,15 +45,17 @@ app.get("/", async (req, res) => {
 });
 
 // POST a new post
+// after the user submits an answer, we get a POST request and we trim the spaces using trim().
 app.post("/submit", (req, res) => {
   let answer = req.body.answer.trim();
   let isCorrect = false;
+    // if the answer is equal to the current question capital, then the score increases by 1.Therefore it increaes by 1 and so on , until the answer is not equal to the capital
   if (currentQuestion.name.toLowerCase() === answer.toLowerCase()) {
     totalCorrect++;
     console.log(totalCorrect);
     isCorrect = true;
   }
-
+// after the prevoius question, the next question will be given by the code below..
   nextQuestion();
   res.render("index.ejs", {
     question: currentQuestion,
@@ -61,7 +66,7 @@ app.post("/submit", (req, res) => {
 
 async function nextQuestion() {
   const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
-
+  // we get a random country and random capital after randomization , we give it to the current question.
   currentQuestion = randomCountry;
 }
 

d69a08631d70a391befc7005e925283a18b03b29
Vishnu Vandhan Talakokkula <talakokkula.vishnu2004@gmail.com>
2025-01-20 12:25:32 +0530

    changes

--- WebDev/Database/WorldCapQuiz/solution.js
+++ WebDev/Database/WorldCapQuiz/solution.js
@@ -1,7 +1,8 @@
+// import the necessary pacakges to run the express app.
 import express from "express";
 import bodyParser from "body-parser";
-import pg from "pg";
-
+import pg from "pg"; // postgres npm
+// postgres syntax for node
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
@@ -9,12 +10,12 @@ const db = new pg.Client({
   password: "123456",
   port: 5432,
 });
-
+// express app
 const app = express();
 const port = 3000;
-
+// for database connection..
 db.connect();
-
+// array of capitals.. using SQL queries to store in the array..
 let quiz = [];
 db.query("SELECT * FROM capitals", (err, res) => {
   if (err) {
@@ -28,10 +29,11 @@ db.query("SELECT * FROM capitals", (err, res) => {
 let totalCorrect = 0;
 
 // Middleware
+// body parser is used to get the data from the form..
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let currentQuestion = {};
+let currentQuestion = {}; // it is equal to a random object
 
 // GET home page
 app.get("/", async (req, res) => {
@@ -42,6 +44,7 @@ app.get("/", async (req, res) => {
 });
 
 // POST a new post
+// after the user submits an answer, we get a POST request and we trim the spaces using trim().
 app.post("/submit", (req, res) => {
   let answer = req.body.answer.trim();
   let isCorrect = false;
@@ -62,6 +65,7 @@ app.post("/submit", (req, res) => {
 async function nextQuestion() {
   const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
   currentQuestion = randomCountry;
+  // we get a random country and random capital after randomization , we give it to the current question.
 }
 
 app.listen(port, () => {
